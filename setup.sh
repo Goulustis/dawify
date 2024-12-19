0df87905c76edd6cf6e4b9736bfcbf5e37ae4e3c
goulustis <cnimaiv@gmail.com>
2024-12-18 18:59:49 -0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ mkdir -p "$INSTALL_DIR"
 aws s3 cp s3://amt-deploy-public/amt/ "$INSTALL_DIR" --no-sign-request --recursive
 cd "$INSTALL_DIR/src"
 python -m pip install -r requirements.txt
+
+# install pytorch; demucs and mt3 could break pytorch versions
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
