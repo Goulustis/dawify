bf29e4dad51ebe104bfbb4e51d388d1d32db6838
goulustis <cnimaiv@gmail.com>
2024-12-18 19:06:28 -0800

    renamed

--- dawify/midify/mt3.py
+++ dawify/midify/mt3.py
@@ -9,7 +9,7 @@ from dawify.midify.mt3_utils import load_model, process_audio
 class MT3Config(InstantiateConfig):
     _target: Type = field(default_factory=lambda: MT3_Mod)
 
-    model_names: Literal["YMT3+", "YPTF+Single (noPS)", "YPTF+Multi (PS)", "YPTF.MoE+Multi (noPS)", "YPTF.MoE+Multi (PS)"] = "YMT3+"
+    model_name: Literal["YMT3+", "YPTF+Single (noPS)", "YPTF+Multi (PS)", "YPTF.MoE+Multi (noPS)", "YPTF.MoE+Multi (PS)"] = "YMT3+"
     """model used for transcribing music -> midi"""
 
     precision: Literal["32", "bf16-mixed", "16"] = "16"
@@ -18,7 +18,7 @@ class MT3Config(InstantiateConfig):
 class MT3_Mod:
     def __init__(self, config: MT3Config):
         self.config = config
-        self.model = load_model(config.model_names, config.precision)
+        self.model = load_model(config.model_name, config.precision)
     
     @torch.no_grad()
     def convert(self, inp_f: str):
