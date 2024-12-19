cc220847b9158704da0eafa08892df5aa13e6a5a
goulustis <cnimaiv@gmail.com>
2024-12-18 17:01:24 -0800

    mkdir

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 ## THIS INSTALL MT3
 INSTALL_DIR="dawify/third_party/amt"
+mkdir -p "$INSTALL_DIR"
 
 aws s3 cp s3://amt-deploy-public/amt/ "$INSTALL_DIR" --no-sign-request --recursive
 cd "$INSTALL_DIR"
