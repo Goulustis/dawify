adafcac29c73ec05cc5d223135dfc0fb66e72c6c
goulustis <cnimaiv@gmail.com>
2024-12-18 17:07:33 -0800

    fix path

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ INSTALL_DIR="dawify/third_party/amt"
 mkdir -p "$INSTALL_DIR"
 
 aws s3 cp s3://amt-deploy-public/amt/ "$INSTALL_DIR" --no-sign-request --recursive
-cd "$INSTALL_DIR"
+cd "$INSTALL_DIR/src"
 python -m pip install -r requirements.txt
