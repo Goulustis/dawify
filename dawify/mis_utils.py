6c86b485559a630c8e5bd9d8211240b4be9dbe55
goulustis <cnimaiv@gmail.com>
2024-12-20 14:01:02 -0800

    print file path

--- dawify/mis_utils.py
+++ dawify/mis_utils.py
@@ -4,6 +4,10 @@ import inspect
 CONSOLE = rich.get_console()
 
 def rprint(inp_str:str):
-    line_num = inspect.currentframe().f_back.f_lineno
-    inp_str = f"[cyan]{line_num}[/cyan]: {inp_str}"
+
+    frame = inspect.currentframe().f_back
+    file_name = frame.f_code.co_filename
+    relative_file_name = file_name.split('dawify', 1)[1][1:]
+    line_num = frame.f_lineno
+    inp_str = f"[cyan]{relative_file_name}:{line_num}[/cyan]: {inp_str}"
     CONSOLE.print(inp_str)
