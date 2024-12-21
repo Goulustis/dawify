78ce38cd71338c09ab524af26deb27ca5b973336
goulustis <cnimaiv@gmail.com>
2024-12-20 23:36:44 -0800

    include script vs cli version

--- dawify/run.py
+++ dawify/run.py
@@ -3,17 +3,29 @@ import tyro
 from dawify.pipeline.pipeline import PipelineConfig, Pipeline
 from dawify.midify.mt3 import MT3Config
 
-def main(config:PipelineConfig, inp_f:str):
-    # config = PipelineConfig(
-    #     mt3_config=MT3Config(
-    #         model_name="YMT3+",
-    #         precision="16"
-    #     )
-    # )
+def main_cli(config:PipelineConfig, inp_f:str):
+    config = PipelineConfig(
+        mt3_config=MT3Config(
+            model_name="YMT3+",
+            precision="16"
+        )
+    )
 
     pipeline:Pipeline = config.setup()
     pipeline.process(inp_f)
-    # pipeline.process("assets/All the Way North.mp3")
+
+
+def main_script():
+    config = PipelineConfig(
+        mt3_config=MT3Config(
+            model_name="YMT3+",
+            precision="16"
+        )
+    )
+
+    pipeline:Pipeline = config.setup()
+    pipeline.process("assets/All the Way North.mp3")
 
 if __name__ == "__main__":
-    tyro.cli(main)
+    main_script()       # Run this script with `python dawify/run.py`
+    tyro.cli(main_cli)    # Run this script with `python dawify/run.py --help`
