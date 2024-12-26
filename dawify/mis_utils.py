b770a9b1d8918f1b2956c0e87b05d00a9be6864b
goulustis <cnimaiv@gmail.com>
2024-12-26 14:12:20 -0800

    add print trace

--- dawify/mis_utils.py
+++ dawify/mis_utils.py
@@ -3,11 +3,14 @@ import inspect
 
 CONSOLE = rich.get_console()
 
-def rprint(inp_str:str):
+def rprint(inp_str:str, print_trace:bool=True):
 
     frame = inspect.currentframe().f_back
-    file_name = frame.f_code.co_filename
-    relative_file_name = file_name.split('dawify', 1)[1][1:]
-    line_num = frame.f_lineno
+
+    relative_file_name, line_num = "", ""
+    if print_trace:
+        line_num = frame.f_lineno
+        file_name = frame.f_code.co_filename
+        relative_file_name = file_name.split('dawify', 1)[1][1:]
     inp_str = f"[cyan]{relative_file_name}:{line_num}[/cyan]: {inp_str}"
     CONSOLE.print(inp_str)
