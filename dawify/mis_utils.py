398b68b3dacf8e7b11bcfb9784ec38ff8ba1dc88
goulustis <cnimaiv@gmail.com>
2025-01-03 17:12:40 -0800

    add utils

--- dawify/mis_utils.py
+++ dawify/mis_utils.py
@@ -1,4 +1,5 @@
 import rich
+from rich.table import Table
 import inspect
 
 CONSOLE = rich.get_console()
@@ -14,3 +15,14 @@ def rprint(inp_str:str, print_trace:bool=True):
         relative_file_name = file_name.split('dawify', 1)[1][1:]
     inp_str = f"[cyan]{relative_file_name}:{line_num}[/cyan]: {inp_str}"
     CONSOLE.print(inp_str)
+
+def print_metrics(metrics: dict, header: tuple = ("Metric", "Value")):
+    table = Table(title="Backtest Metrics")
+
+    table.add_column(header[0], justify="right", style="cyan", no_wrap=True)
+    table.add_column(header[1], style="magenta")
+
+    for metric, value in metrics.items():
+        table.add_row(metric, f"{value:.4f}")
+
+    CONSOLE.print(table)
